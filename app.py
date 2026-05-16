c26b70a5568c252a1557a6fb10b0759a91930c9e
usiambhai2006 <siamsell001@gmail.com>
2026-05-16 17:16:16 +0600

    Update app.py

--- app.py
+++ app.py
@@ -28,5 +28,8 @@ def send_like():
             "error": str(e)
         })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
